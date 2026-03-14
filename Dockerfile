22c793e72dfb3eee46a656fe162f2102f29c622f
Shreyans Joshi <shreyjosh@gmail.com>
2026-03-14 12:15:53 +0530

    Added dockerfile for log generator and k8s config

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,4 @@ WORKDIR /app
 COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 COPY ./log_generator.py .
-CMD ["python", "log_generator.py"]
+CMD ["python", "-u", "log_generator.py"]
--- log_generator.py
+++ log_generator.py
@@ -7,6 +7,7 @@ from openai import OpenAI
 
 def get_log_text():
     """Fetch log messages from the OpenAI API and write them to a file."""
+    print("Fetching log messages from OpenAI API...", flush=True)
     client = OpenAI(
         api_key=os.environ.get("OPENAI_API_KEY"),
         base_url="https://api.groq.com/openai/v1",
@@ -16,13 +17,12 @@ def get_log_text():
         input="Create 10 log messages from one of the type INFO, WARNING, ERROR, DEBUG, CRITICAL, EXCEPTION, ALERT, EMERGENCY. Keep it concise and relevant to software development. Do not write anything else other than the log messages. Do not include any explanations or additional text. Just the log messages in the specified format. Keep them short and to the point. Keep all the messages in a single response, separated by newlines and under 500 tokens. Keep the language english only. Use the format and only this format =  [timestamp] [log_type]: [message].",
         model="allam-2-7b",
     )
-    with open("./logs/logs.txt", "a", encoding="utf-8") as f:
-        f.write(response.output_text + "\n")
-    return
+    return(response.output_text)
 
 def main():
     """Main function to generate logs and sleep for a random interval."""
-    get_log_text()
+    print("Generating logs...", flush=True)
+    print(get_log_text(), flush=True)
     sleep_time = random.uniform(10, 30)
     time.sleep(sleep_time)
 
